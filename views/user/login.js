efa92a5f948cb955cb9874760b2edcbab0015b04
bluehawky <woshipmx1991@sina.com>
2014-10-06 10:02:33 +0800

    markdown write feature added

--- views/user/login.js
+++ views/user/login.js
@@ -14,6 +14,23 @@ function inputInvoke(req,res,feature){
     console.log("body",JSON.stringify(req.body));
     console.log("userId: "+ userId);
     console.log("passwdMD5: "+ passwdMD5);
+    var clientIp=getClientIp(req);
+    if (clientIp == '127.0.0.1'){
+        console.log('ok');
+    }
+//    console.log("client ip: "+ clientIp);
+}
+function getClientIp(req) {
+    var ipAddress;
+    var forwardedIpsStr = req.header('x-forwarded-for');
+    if (forwardedIpsStr) {
+        var forwardedIps = forwardedIpsStr.split(',');
+        ipAddress = forwardedIps[0];
+    }
+    if (!ipAddress) {
+        ipAddress = req.connection.remoteAddress;
+    }
+    return ipAddress;
 }
 exports.inputInvoke=inputInvoke;
 exports.invoke=invoke;
