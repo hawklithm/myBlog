d1f73d12043919f2189b1f911c423c64902397a0
bluehawky <woshipmx1991@sina.com>
2014-10-08 23:58:11 +0800

    add bootstrap backend

--- views/resource/js/article-content-index.js
+++ views/resource/js/article-content-index.js
@@ -5,26 +5,4 @@ function redirectToArticle(obj){
     console.log(obj.id);
     window.location.assign('/article_show?code='+obj.id);
 }
-//function postData(form , url ) {
-//    var XHR = new XMLHttpRequest();
-//
-//    // We bind the FormData object and the form element
-//    var FD  = new FormData(form);
-//
-//    // We define what will happen if the data are successfully sent
-//    XHR.addEventListener("load", function(event) {
-//        alert(event.target.responseText);
-//    });
-//
-//    // We define what will happen if case of error
-//    XHR.addEventListener("error", function(event) {
-//        alert('Oups! Something goes wrong.');
-//    });
-//
-//    // We setup our request
-//    XHR.open("POST", url);
-//
-//    // The data send are the one the user provide in the form
-//    XHR.send(FD);
-//}
-//
+
