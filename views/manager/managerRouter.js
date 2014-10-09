95a11f66ef9bdf213b33f8115552d6325003980d
bluehawky <woshipmx1991@sina.com>
2014-10-09 23:51:16 +0800

    page rebuild

--- views/manager/managerRouter.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by bluehawky on 14-10-9.
- */
--- views/resource/javascript/article-content-index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by bluehawky on 14-9-27.
- */
-function redirectToArticle(obj){
-    console.log(obj.id);
-    window.location.assign('/article_show?code='+obj.id);
-}
